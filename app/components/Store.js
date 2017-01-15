fd8c3717941b56d258888b06fa6610168345a6df
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-01-15 16:43:35 -0500

    Refactored store layout

--- app/components/Store.js
+++ app/components/Store.js
@@ -1,16 +1,24 @@
 
 import React from 'react'
-import { Grid } from 'react-bootstrap'
+import { Grid, Row, Col, Thumbnail } from 'react-bootstrap'
 
 import StoreCarousel from 'components/StoreCarousel'
-import StoreItems from 'components/StoreItems'
+import cImage from 'img/thumbnail.png'
 
 let Store = () => {
   return (
     <div className="store-container">
-      <Grid>
-        <StoreCarousel />
-        <StoreItems />
+      <StoreCarousel />
+      <Grid className="store-items">
+        { [0,1].map((i) => (
+          <Row key={i}>
+            { [0,1,2,3].map(j => (
+              <Col key={j} xs={12} md={6} lg={3} className="top-buffer">
+                <Thumbnail href="#" src={cImage} />
+              </Col>
+            ))}
+          </Row>
+        ))}
       </Grid>
     </div>
   )
--- app/components/StoreCarousel.js
+++ app/components/StoreCarousel.js
@@ -6,23 +6,23 @@ import cImage from 'img/content/store-slider/carousel.png'
 
 export default () => {
   return (
-    <Carousel>
+    <Carousel className="store-carousel container">
       <Carousel.Item>
-        <img width={1140} height={600} src={cImage}/>
+        <img width={1170} height={600} src={cImage}/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img width={1140} height={600} src={cImage}/>
+        <img width={1170} height={600} src={cImage}/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img width={1140} height={600} src={cImage}/>
+        <img width={1170} height={600} src={cImage}/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
