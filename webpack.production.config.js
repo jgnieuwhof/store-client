de10630c6967e87daa0da7f8e8cccf3ba113ccd2
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-03-08 01:05:20 -0500

    Forgot to update prod config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -48,6 +48,7 @@ module.exports = {
        helpers: 'app/helpers',
        reducers: 'app/reducers',
        img: 'app/img',
+       util: 'app/util',
      },
      extensions: ['', '.js', '.scss'],
   },
