ee939c2709e606de1fe7ee61fd73dbdc6dbdf42d
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2017-11-29 17:34:41 -0500

    Added nav autoclose for mobile

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Navbar,
   Nav,
@@ -9,32 +9,40 @@ import { Link } from 'react-router'
 import CartBadge from './CartBadge'
 import NavItem from './NavItem'
 
-let Header = () => {
-  return (
-    <div className="header-container">
-      <Navbar inverse fixedTop>
-        <Navbar.Header>
-          <Navbar.Brand className='showguide'>
-            <Link to="/store"><span>RAMBLE ON</span></Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="/store">Store</NavItem>
-            <NavItem eventKey={2} href="/about">About</NavItem>
-            <NavItem eventKey={3} href="/contact">Contact</NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={4} href="/signup">Sign Up</NavItem>
-            <NavItem eventKey={5} href="/cart">
-              <CartBadge />
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  )
+class Header extends Component {
+  state = { expanded: false }
+  setExpanded = expanded => this.setState({ expanded })
+  closeNav = () => this.setState({ expanded: false })
+  render() {
+    return (
+      <div className="header-container">
+        <Navbar inverse fixedTop
+          onToggle={this.setExpanded}
+          expanded={this.state.expanded}
+        >
+          <Navbar.Header>
+            <Navbar.Brand className='showguide'>
+              <Link to="/store"><span>RAMBLE ON</span></Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav onClick={this.closeNav}>
+              <NavItem eventKey={1} href="/store">Store</NavItem>
+              <NavItem eventKey={2} href="/about">About</NavItem>
+              <NavItem eventKey={3} href="/contact">Contact</NavItem>
+            </Nav>
+            <Nav onClick={this.closeNav} pullRight>
+              <NavItem eventKey={4} href="/signup">Sign Up</NavItem>
+              <NavItem eventKey={5} href="/cart">
+                <CartBadge />
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default Header
